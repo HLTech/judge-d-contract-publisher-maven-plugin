dc6991e6cf7e60b8e7f303775694b2162895d867
garlicsauce <adrian.michalik@wp.pl>
2019-09-01 21:17:58 +0200

    Updating secring - encrypted on unix

--- deploy-to-maven-central.sh
+++ deploy-to-maven-central.sh
@@ -1,5 +1,5 @@
 echo "decrypting secring.gpg..."
-openssl aes-256-cbc -K $encrypted_3362897dd6fb_key -iv $encrypted_3362897dd6fb_iv -in $TRAVIS_BUILD_DIR/secring.gpg.enc -out $TRAVIS_BUILD_DIR/secring.gpg -d
+openssl aes-256-cbc -K $encrypted_a89191c56639_key -iv $encrypted_a89191c56639_iv -in $TRAVIS_BUILD_DIR/secring.gpg.enc -out $TRAVIS_BUILD_DIR/secring.gpg -d
 
 echo "publish archives & closeAndPromoteRepository..."
 ./gradlew build publishToNexus closeAndReleaseRepository -Psigning.keyId=$signingKeyId -Psigning.password=$secretKeyPassword -Psigning.secretKeyRingFile=$TRAVIS_BUILD_DIR/secring.gpg --info --stacktrace
