3817f79f6ef2c95bc88fd8c5f61fe15b72ee8e92
Mateusz Urbański <mateusz.urbanski@hltech.com>
2019-09-04 13:04:02 +0200

    Release configuration

--- deploy-to-maven-central.sh
+++ deploy-to-maven-central.sh
@@ -1,5 +1,6 @@
 echo "decrypting secring.gpg..."
 openssl aes-256-cbc -K $encrypted_a89191c56639_key -iv $encrypted_a89191c56639_iv -in $TRAVIS_BUILD_DIR/secring.gpg.enc -out $TRAVIS_BUILD_DIR/secring.gpg -d
 
-echo "publish archives & closeAndPromoteRepository..."
-./gradlew build publishToNexus closeAndReleaseRepository -Psigning.keyId=$signingKeyId -Psigning.password=$secretKeyPassword -Psigning.secretKeyRingFile=$TRAVIS_BUILD_DIR/secring.gpg --info --stacktrace
+echo "publish..."
+gpg --import $TRAVIS_BUILD_DIR/secring.gpg
+mvn --settings .travis/settings.xml deploy -Prelease -Dgpg.passphrase=$secretKeyPassword
